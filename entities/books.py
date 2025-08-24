91276bd9c12786c50616e5667fcc53d7db6b9166
CvetiGofreti <cvetka012ilieva@gmail.com>
2025-08-24 21:40:34 +0300

    code refactor books

--- entities/books.py
+++ entities/books.py
@@ -1,67 +1,74 @@
-import pygame
 import os
+
+import pygame
 
 from others import TileType
 from entities import Facing
 
-tileSize = 64
+TILE_SIZE = 64
 
 class MovableBooks:
     def __init__(self, x, y, image):
         self.image = image
-        self.rect = pygame.Rect(x * tileSize, y * tileSize, tileSize, tileSize)
+        self.rect = pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
         self.velocity = 0
         self.gravity = 900
-        self.onGround = False
-        self.screenWidth, self.screenHeight = pygame.display.get_surface().get_size()
-        self.playerPushing = None
+        self.on_ground = False
+        self.screen_width, self.screen_height = pygame.display.get_surface().get_size()
+        self.player_pushing = None
 
     def update(self, dt, grid, players):
-        self.playerPushing = None
+        self.player_pushing = None
 
-        if not self.onGround:
+        if not self.on_ground:
             self.velocity += self.gravity * dt
         self.rect.y += self.velocity * dt
-        self.onGround = False
+        self.on_ground = False
         for x, y in self.get_overlapping_tiles():
             if grid[y][x] == TileType.FLOOR:
-                floorRect = pygame.Rect(x * tileSize, y * tileSize, tileSize, 15)
-                if self.rect.colliderect(floorRect):
-                    self.rect.bottom = floorRect.top
+                floor_rect = pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, 15)
+                if self.rect.colliderect(floor_rect):
+                    self.rect.bottom = floor_rect.top
                     self.velocity = 0
-                    self.onGround = True
-            
-        pushOffset = 16
+                    self.on_ground = True
+
+        push_offset = 16
         for player in players:
             if player.rect.colliderect(self.rect):
-                if player.facing == Facing.RIGHT and player.rect.right <= self.rect.left + pushOffset:
+                if (
+                    player.facing == Facing.RIGHT
+                    and player.rect.right <= self.rect.left + push_offset
+                    ):
                     self.rect.x += player.speed * dt
-                    self.playerPushing = player
-                elif player.facing == Facing.LEFT and player.rect.left >= self.rect.right - pushOffset:
+                    self.player_pushing = player
+                elif (
+                    player.facing == Facing.LEFT
+                    and player.rect.left >= self.rect.right - push_offset
+                    ):
                     self.rect.x -= player.speed * dt
-                    self.playerPushing = player
-        
-        if self.rect.bottom > self.screenHeight:
-            self.rect.bottom = self.screenHeight
+                    self.player_pushing = player
+
+        if self.rect.bottom > self.screen_height:
+            self.rect.bottom = self.screen_height
             self.velocity = 0
-            self.onGround = True
+            self.on_ground = True
 
         if self.rect.left < 0:
             self.rect.left = 0
-            if(self.playerPushing.facing == Facing.LEFT):
-                self.playerPushing.movingLeft = False
+            if self.player_pushing.facing == Facing.LEFT:
+                self.player_pushing.movingLeft = False
         if self.rect.right > pygame.display.get_surface().get_width():
             self.rect.right = pygame.display.get_surface().get_width()
-            if(self.playerPushing.facing == Facing.RIGHT):
-                self.playerPushing.movingRight = False
+            if self.player_pushing.facing == Facing.RIGHT:
+                self.player_pushing.movingRight = False
 
 
     def get_overlapping_tiles(self):
         tiles = []
-        left = self.rect.left // tileSize
-        right = (self.rect.right - 1) // tileSize
-        top = self.rect.top // tileSize
-        bottom = (self.rect.bottom - 1) // tileSize
+        left = self.rect.left // TILE_SIZE
+        right = (self.rect.right - 1) // TILE_SIZE
+        top = self.rect.top // TILE_SIZE
+        bottom = (self.rect.bottom - 1) // TILE_SIZE
 
         for y in range(top, bottom + 1):
             for x in range(left, right + 1):
