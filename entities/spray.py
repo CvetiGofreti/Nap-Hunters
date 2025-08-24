c39f5ed780b83b3bd0cf63fc41028df1d6fcfbe2
CvetiGofreti <cvetka012ilieva@gmail.com>
2025-08-24 21:34:06 +0300

    CODE REFACTOR SPRAY

--- entities/spray.py
+++ entities/spray.py
@@ -1,35 +1,44 @@
-import pygame
 import os
+
+import pygame
+
 from others import SprayType
 
-tileSize = 64
+TILE_SIZE = 64
 
 class Spray:
     def __init__(self, x, y, height, assets):
-        self.baseX = x
-        self.baseY = y
+        self.base_x = x
+        self.base_y = y
         self.height = height
         self.assets = assets
         self.active = True
-        self.collideRect = pygame.Rect(x * tileSize, (y - height + 1) * tileSize, tileSize, height * tileSize)
+        self.collide_rect = pygame.Rect(
+            x * TILE_SIZE,
+            (y - height + 1) * TILE_SIZE,
+            TILE_SIZE, height * TILE_SIZE
+        )
 
-    def update(self, anyButtonPressed, players):
-        self.active = not anyButtonPressed
+    def update(self, any_button_pressed, players):
+        self.active = not any_button_pressed
 
         for player in players:
-            if self.active and self.collideRect.colliderect(player.rect):
+            if self.active and self.collide_rect.colliderect(player.rect):
                 player.respawn()
 
     def draw(self, screen):
         if os.getenv("DEBUG") == "1":
-            pygame.draw.rect(screen, pygame.Color("red"), self.collideRect, 2)
+            pygame.draw.rect(screen, pygame.Color("red"), self.collide_rect, 2)
 
+        draw_position = (self.base_x * TILE_SIZE, self.base_y * TILE_SIZE)
         if not self.active:
-            screen.blit(self.assets.sprays[SprayType.OFF], (self.baseX * tileSize, self.baseY * tileSize))
+            screen.blit(self.assets.sprays[SprayType.OFF], draw_position)
         elif self.height == 1:
-            screen.blit(self.assets.sprays[SprayType.ON], (self.baseX * tileSize, self.baseY * tileSize))
+            screen.blit(self.assets.sprays[SprayType.ON], draw_position)
         else:
-            screen.blit(self.assets.sprays[SprayType.ON_BOTTOM], (self.baseX * tileSize, self.baseY * tileSize))
+            screen.blit(self.assets.sprays[SprayType.ON_BOTTOM], draw_position)
             for i in range(1, self.height - 1):
-                screen.blit(self.assets.sprays[SprayType.ON_MIDDLE], (self.baseX * tileSize, (self.baseY - i) * tileSize))
-            screen.blit(self.assets.sprays[SprayType.ON_TOP], (self.baseX * tileSize, (self.baseY - self.height + 1) * tileSize))
+                draw_position = (self.base_x * TILE_SIZE, (self.base_y - i) * TILE_SIZE)
+                screen.blit(self.assets.sprays[SprayType.ON_MIDDLE], draw_position)
+            draw_position = (self.base_x * TILE_SIZE, (self.base_y - self.height + 1) * TILE_SIZE)
+            screen.blit(self.assets.sprays[SprayType.ON_TOP], draw_position)
