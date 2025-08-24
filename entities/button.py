c395025c05b6cf9068fa1d772594f0a2e2e1fc38
CvetiGofreti <cvetka012ilieva@gmail.com>
2025-08-24 21:42:41 +0300

    CODE REFACTOR BUTTON

--- entities/button.py
+++ entities/button.py
@@ -1,29 +1,35 @@
-import pygame
 import os
+
+import pygame
 
 from others import TileType
 
-tileSize = 64
+TILE_SIZE = 64
 
 class Button:
     def __init__(self, x, y, assets):
         self.image = assets.entities[TileType.BUTTON]
-        self.imagePressed = assets.button_pressed
-        self.rect = pygame.Rect(x * tileSize, y * tileSize, tileSize, tileSize)
-        collideOffset = 32
-        self.collideRect = pygame.Rect(x * tileSize + collideOffset // 2, y * tileSize + collideOffset, tileSize - collideOffset, tileSize - collideOffset)
+        self.image_pressed = assets.button_pressed
+        self.rect = pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+        collide_offset = 32
+        self.collide_rect = pygame.Rect(
+            x * TILE_SIZE + collide_offset // 2,
+            y * TILE_SIZE + collide_offset,
+            TILE_SIZE - collide_offset,
+            TILE_SIZE - collide_offset
+        )
 
     def is_pressed(self, players):
         for player in players:
-            if self.collideRect.colliderect(player.rect):
+            if self.collide_rect.colliderect(player.rect):
                 return True
         return False
 
     def draw(self, screen, players):
-        if(self.is_pressed(players)):
-            screen.blit(self.imagePressed, self.rect.topleft)
+        if self.is_pressed(players):
+            screen.blit(self.image_pressed, self.rect.topleft)
         else:
             screen.blit(self.image, self.rect.topleft)
 
         if os.getenv("DEBUG") == "1":
-            pygame.draw.rect(screen, pygame.Color("red"), self.collideRect, 2)
+            pygame.draw.rect(screen, pygame.Color("red"), self.collide_rect, 2)
