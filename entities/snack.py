d7e31fc195eaac632bcd85fbee89497e3f7cb233
CvetiGofreti <cvetka012ilieva@gmail.com>
2025-08-24 21:36:13 +0300

    refactor snack

--- entities/snack.py
+++ entities/snack.py
@@ -1,33 +1,34 @@
-import pygame
 import os
 
-tileSize = 64
+import pygame
+
+TILE_SIZE = 64
 
 class Snack:
     def __init__(self, x, y, image):
-        self.x = x 
+        self.x = x
         self.y = y
         self.image = image
         self.rect = pygame.Rect(
-            x * tileSize,
-            y * tileSize,
-            tileSize,
-            tileSize
+            x * TILE_SIZE,
+            y * TILE_SIZE,
+            TILE_SIZE,
+            TILE_SIZE
         )
 
-        collisionSize = 32
-        offset = (tileSize - collisionSize) // 2
-        self.collideRect = pygame.Rect(
+        collision_size = 32
+        offset = (TILE_SIZE - collision_size) // 2
+        self.collide_rect = pygame.Rect(
             self.rect.x + offset,
             self.rect.y,
-            collisionSize,
-            collisionSize
+            collision_size,
+            collision_size
         )
 
     def draw(self, screen):
         screen.blit(self.image, self.rect.topleft)
         if os.getenv("DEBUG") == "1":
-            pygame.draw.rect(screen, pygame.Color("red"), self.collideRect, 2)
+            pygame.draw.rect(screen, pygame.Color("red"), self.collide_rect, 2)
 
-    def is_colliding_with(self, playerRect):
-        return self.collideRect.colliderect(playerRect)
+    def is_colliding_with(self, player_rect):
+        return self.collide_rect.colliderect(player_rect)
